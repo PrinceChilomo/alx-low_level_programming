7d1f231d439ffacc804b683cf45f027be02ad2a4
PrinceChilomo <chilomo2019@gmail.com>
2022-09-14 10:30:28 -0700

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - function that prints the last digit of a number
- * @n: the number in question
+ * @n: The number in question
  * Return: 0
  */
 int print_last_digit(int n)
@@ -11,7 +11,8 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 		last_digit *= -1;
-	_putchar(last_digit + '0';
+
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
