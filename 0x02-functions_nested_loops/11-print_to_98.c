bedc604ddf66ca96347da2f10f87664340620694
PrinceChilomo <chilomo2019@gmail.com>
2022-09-14 12:29:40 -0700

    t11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,6 +4,7 @@
  * print_to_98 - print all natural numbers from  input to 98,
  * 		in order separated by a comma followed by a space.
  * @n: The number to begin counting at.
+ * Return: 0
  */
 void print_to_98(int n)
 {
