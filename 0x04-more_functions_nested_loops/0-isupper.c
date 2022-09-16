3e3befed9aadf316bf13e4a58af4813d200722d5
PrinceChilomo <chilomo2019@gmail.com>
2022-09-16 12:35:09 -0700

    t0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character isupcase or not
+ * @c: the character to be tested
+ * Return: 1 otherwise o
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (C <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
